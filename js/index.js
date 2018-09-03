66159b51bd93b0462f320d7690e92cff4232c618
konglingkai <konglingkai@xylink.com>
2018-09-03 10:10:58 +0800

    发送图片，暂时有bug

--- js/index.js
+++ js/index.js
@@ -2,37 +2,36 @@ const Vue = require('./js/vue')
 const messages = require("./js/message_pb")
 const $ = require('jquery')
 const uuid = require('uuid')
-const {ipcRenderer, remote} = require('electron')
+const {ipcRenderer, remote, shell} = require('electron')
+const os = require('os')
+const fs = require('fs')
 
 let sharedObject = remote.getGlobal("sharedObject")
 let client = sharedObject.client
 const modelData = {
     user: {
-        'userId':'',
-        'nickname':'',
-        'imgUrl':''
+        'userId': '',
+        'nickname': '',
+        'imgUrl': ''
     },
     chatPerson: {
-        'destId':'',
-        'nickname':'',
-        'imgUrl':''
+        'destId': '',
+        'nickname': '',
+        'imgUrl': ''
     },
     conversations: [],
     messages: [],
     pageSize: 30,
     scrollEnd: false,
-    destId2Message: {
-
-    },
-    unReadMsgCnt: {
-
-    },
+    destId2Message: {},
+    unReadMsgCnt: {},
     is_fresh: false,
     isShow: true,//会话列表展示
     isShow2: false,//好友列表展示
     isOpen: false,//联系人列表展示
     isOpenDetail: false,//联系人详细信息展示
-    isFloat: false//用户详情展示
+    isFloat: false,//用户详情展示
+    filepaths:[]
 };
 
 let header = {
@@ -44,34 +43,59 @@ let header = {
 let vm = new Vue({
     el: '#app',
     data: modelData,
-    created(){
+    created() {
         this.init();
     },
     methods: {
-        scrollEvent: function(event) {
-            if(event.srcElement.scrollTop <= 10 && !modelData.scrollEnd){
+        scrollEvent: function (event) {
+            if (event.srcElement.scrollTop <= 10 && !modelData.scrollEnd) {
                 let conversation = {'destId': this.chatPerson.destId, 'msgId': modelData.messages[0].msgId}
                 this.showHistoryMessage(conversation);
             }
         },
         sendMsg: function (event) {
             event.preventDefault()
-            let content = $('.content').val()
-            let message = new messages.ProtocolMessage()
-            let req = new messages.ProtocolMessage.TRequest()
-            req.setReqtype(messages.ProtocolMessage.RequestType.CHAT)
-            let chat = new messages.CPrivateChat()
-            chat.setMsgid(uuid.v1())
-            chat.setContent(Buffer.from(content))
-            chat.setDestid(this.chatPerson.destId)
-            chat.setUserid(this.user.userId)
-            chat.setChattype(messages.CPrivateChat.ChatType.ONE2ONE)
-            chat.setDatatype(messages.CPrivateChat.DataType.TXT)
-            req.setChat(chat)
-            message.setRequest(req)
-            let bytes = message.serializeBinary()
-            client.write(Buffer.from(bytes))
-            $('.content').val('')
+            let content = ''
+            if($('.img-area img').length > 0) {
+                for(let i in modelData.filepaths) {
+                    let filepath = modelData.filepaths[i]
+                    let filemsg = new messages.ProtocolMessage()
+                    let filereq = new messages.ProtocolMessage.TRequest()
+                    filereq.setReqtype(messages.ProtocolMessage.RequestType.CHAT)
+                    let filechat = new messages.CPrivateChat()
+                    let data = fs.readFileSync(filepath,'binary')
+                    filechat.setContent(Buffer.from(data))
+                    filechat.setDestid(this.chatPerson.destId)
+                    filechat.setUserid(this.user.userId)
+                    filechat.setChattype(messages.CPrivateChat.ChatType.ONE2ONE)
+                    filechat.setDatatype(messages.CPrivateChat.DataType.IMG)
+                    filechat.setExtname(filepath.substring(filepath.lastIndexOf('\.')+1))
+                    filereq.setChat(filechat)
+                    filemsg.setRequest(filereq)
+                    let bytes = filemsg.serializeBinary()
+                    client.write(bytes)
+                    content = '图片'
+                    // fs.close()
+                }
+            }else{
+                let message = new messages.ProtocolMessage()
+                let req = new messages.ProtocolMessage.TRequest()
+                req.setReqtype(messages.ProtocolMessage.RequestType.CHAT)
+                let chat = new messages.CPrivateChat()
+                chat.setMsgid(uuid.v1())
+                content = $('.content textarea').val()
+                chat.setContent(Buffer.from(content))
+                chat.setDestid(this.chatPerson.destId)
+                chat.setUserid(this.user.userId)
+                chat.setChattype(messages.CPrivateChat.ChatType.ONE2ONE)
+                chat.setDatatype(messages.CPrivateChat.DataType.TXT)
+                req.setChat(chat)
+                message.setRequest(req)
+                let bytes = message.serializeBinary()
+                client.write(Buffer.from(bytes))
+            }
+            $('.content textarea').val('')
+            $('.img-area').empty()
             let m = {
                 "msgId": this.chatPerson.msgId,
                 "sendId": this.user.userId,
@@ -87,7 +111,7 @@ let vm = new Vue({
             let path = '/user/profile/' + sharedObject.userId + '/' + sharedObject.certificate
             $.get({
                 url: sharedObject.url + path,
-                timeout:sharedObject.timeout,
+                timeout: sharedObject.timeout,
                 headers: header
             }).done(function (res) {
                 modelData.user.userId = res.userId
@@ -98,19 +122,19 @@ let vm = new Vue({
             })
         },
         getConversations: function () {
-            let path = '/user/conversation/'+sharedObject.userId
+            let path = '/user/conversation/' + sharedObject.userId
             $.get({
-                url: sharedObject.url+path,
-                timeout:sharedObject.timeout,
-                headers:header
+                url: sharedObject.url + path,
+                timeout: sharedObject.timeout,
+                headers: header
             }).done(function (res) {
                 modelData.conversations = res;
-                if(!modelData.is_fresh) {
-                    for(let i in res) {
+                if (!modelData.is_fresh) {
+                    for (let i in res) {
                         let c = res[i]
                         modelData.destId2Message[c.destId] = []
                     }
-                    vm.$nextTick(function(){
+                    vm.$nextTick(function () {
                         this.showHistoryMessageByClick(modelData.conversations[0])
                     })
                     modelData.is_fresh = true
@@ -121,14 +145,14 @@ let vm = new Vue({
             this.chatPerson.destId = conversation.destId;
             this.chatPerson.imgUrl = conversation.imgUrl;
             this.chatPerson.nickname = conversation.nickname;
-            if(modelData.destId2Message[conversation.destId].length == 0) {
-                let path = '/user/historymessage/'+sharedObject.userId+'/'+conversation.destId+'/'+conversation.msgId+'/'+modelData.pageSize+'?'+'direct=-2'
+            if (modelData.destId2Message[conversation.destId].length == 0) {
+                let path = '/user/historymessage/' + sharedObject.userId + '/' + conversation.destId + '/' + conversation.msgId + '/' + modelData.pageSize + '?' + 'direct=-2'
                 $.get({
-                    url: sharedObject.url+path,
-                    timeout:sharedObject.timeout,
-                    headers:header
+                    url: sharedObject.url + path,
+                    timeout: sharedObject.timeout,
+                    headers: header
                 }).done(function (res) {
-                    if(res == null || res == []){
+                    if (res == null || res == []) {
                         modelData.scrollEnd = true
                     } else {
                         modelData.messages = modelData.destId2Message[conversation.destId] = res.reverse()
@@ -140,64 +164,64 @@ let vm = new Vue({
                 modelData.messages = this.destId2Message[conversation.destId]
             }
             vm.scrollToEnd()
-            modelData.unReadMsgCnt[conversation.destId]=0
+            modelData.unReadMsgCnt[conversation.destId] = 0
         },
         showHistoryMessage: function (conversation) {
-            let path = '/user/historymessage/'+sharedObject.userId+'/'+conversation.destId+'/'+conversation.msgId+'/'+modelData.pageSize+'?'+'direct=-1'
+            let path = '/user/historymessage/' + sharedObject.userId + '/' + conversation.destId + '/' + conversation.msgId + '/' + modelData.pageSize + '?' + 'direct=-1'
             $.get({
-                url: sharedObject.url+path,
-                timeout:sharedObject.timeout,
-                headers:header
+                url: sharedObject.url + path,
+                timeout: sharedObject.timeout,
+                headers: header
             }).done(function (res) {
-                if(res == null || res == []){
+                if (res == null || res == []) {
                     modelData.scrollEnd = true
                 } else {
                     let r = res.reverse()
                     modelData.destId2Message[conversation.destId] = r.concat(modelData.destId2Message[conversation.destId])
                 }
-                modelData.messages =  modelData.destId2Message[conversation.destId]
+                modelData.messages = modelData.destId2Message[conversation.destId]
             })
         },
-        scrollToEnd:function(){
-            vm.$nextTick(function(){
-                $('.chat-area')[0].scrollTop=$('.chat-area')[0].scrollHeight;   //这样就能将事件执行在界面渲染之后啦
+        scrollToEnd: function () {
+            vm.$nextTick(function () {
+                $('.chat-area')[0].scrollTop = $('.chat-area')[0].scrollHeight;   //这样就能将事件执行在界面渲染之后啦
             })
         },
-        unReadMsgCount: function(sendId){
-            for(let i in modelData.conversations){
+        unReadMsgCount: function (sendId) {
+            for (let i in modelData.conversations) {
                 let c = modelData.conversations[i];
-                if(c.destId == sendId && sendId != modelData.chatPerson.destId){
+                if (c.destId == sendId && sendId != modelData.chatPerson.destId) {
                     let unReadMsgCnt = modelData.unReadMsgCnt[sendId]
-                    if(unReadMsgCnt == undefined || unReadMsgCnt == null){
+                    if (unReadMsgCnt == undefined || unReadMsgCnt == null) {
                         modelData.unReadMsgCnt[sendId] = 1
-                    }else {
-                        if(modelData.unReadMsgCnt[sendId] < 99 && modelData.unReadMsgCnt[sendId] != '99+'){
+                    } else {
+                        if (modelData.unReadMsgCnt[sendId] < 99 && modelData.unReadMsgCnt[sendId] != '99+') {
                             modelData.unReadMsgCnt[sendId]++
-                        }else{
+                        } else {
                             modelData.unReadMsgCnt[sendId] = '99+'
                         }
                     }
                 }
             }
         },
-        flash:function(){
+        flash: function () {
             let curWin = remote.getCurrentWindow()
             curWin.showInactive();
             curWin.flashFrame(true);
         },
-        updateShow: function(data){
+        updateShow: function (data) {
             this.isShow = data.isShow;
             this.isShow2 = data.isShow2;
             this.isOpen = false;//关闭联系人列表
             this.isOpenDetail = false;//关闭联系人详情
         },
-        openContact: function(data){
+        openContact: function (data) {
             this.isOpen = !data;
         },
-        openDetail: function(){
+        openDetail: function () {
             this.isOpenDetail = true
         },
-        sendMsgFromRelationship: function(){
+        sendMsgFromRelationship: function () {
             let d = {
                 isShow: true,
                 isShow2: false
@@ -205,10 +229,22 @@ let vm = new Vue({
             this.updateShow(d)
             //TODO 会话列表第一条展示当前好友会话
         },
-        showUserDtl: function(data){
+        showUserDtl: function (data) {
             this.isFloat = data
         },
-        init:function () {
+        propFile: function () {
+            remote.dialog.showOpenDialog(remote.getCurrentWindow(), {
+                filters: [
+                    {name: '图片', extensions: ['jpg', 'png', 'gif']},
+                    {name: '所有文件（*.*）', extensions: ['*']}],
+                properties: ['openFile', 'multiSelections']
+            }, function (filePaths) {
+                $('.img-area').append("<img class='send-img' src='"+filePaths[0]+"'>")
+                 console.log(filePaths)
+                modelData.filepaths = filePaths
+            });
+        },
+        init: function () {
             this.getUserProfile()
             this.getConversations()
 
@@ -220,9 +256,9 @@ client.on('data', function (bytes) {
     let message = messages.ProtocolMessage.deserializeBinary(bytes)
     console.log(message)
     let response = message.getResponse()
-    if(response.getResptype() == messages.ProtocolMessage.RequestType.LOGIN){
+    if (response.getResptype() == messages.ProtocolMessage.RequestType.LOGIN) {
         let resp = response.getResp();
-        if (resp.getCode()== 200) {
+        if (resp.getCode() == 200) {
             sharedObject.userId = resp.getUserid()
             sharedObject.certificate = resp.getCertificate()
             sharedObject.username = modelData.userId
@@ -241,18 +277,18 @@ client.on('data', function (bytes) {
     }
     modelData.destId2Message[m.sendId].push(m)
     vm.scrollToEnd()
- 	vm.getConversations()
+    vm.getConversations()
     vm.unReadMsgCount(m.sendId)
     vm.flash()
 
 })
-let reconnet_count = 3
+let reconnet_count = 0
 client.on('close', function () {
     reconnet_count--
-    if(reconnet_count <= 0)
+    if (reconnet_count <= 0)
         return
     console.log("connection closed")
-    client.connect(sharedObject.tcpport, sharedObject.host,function () {
+    client.connect(sharedObject.tcpport, sharedObject.host, function () {
         console.log("断线后重连")
         let message = new messages.ProtocolMessage()
         let req = new messages.ProtocolMessage.TRequest()
@@ -270,14 +306,16 @@ client.on('close', function () {
         client.write(Buffer.from(bytes))
     })
 })
-let leftWidth=60, median=252, topHeight=60, bottomHeight=130
-$(function(){
-    $(".left, .median, .right").height($(window).height()-1);
-});
-window.onresize=function () {
-    $(".left, .median, .right").height($(window).height()-1);
-    $(".right").width($(window).width()-leftWidth-median-1);
-    $(".chat-area").height($(window).height()-topHeight-bottomHeight-1)
+let leftWidth = 60, median = 252, topHeight = 60, bottomHeight = 130
+
+$(function () {
+    $(".left, .median, .right").height($(window).height())
+})
+
+window.onresize = function () {
+    $(".left, .median, .right").height($(window).height());
+    $(".right").width($(window).width() - leftWidth - median );
+    $(".chat-area").height($(window).height() - topHeight - bottomHeight)
 }
 
 
